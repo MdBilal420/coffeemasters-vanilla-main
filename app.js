fa7e1f157fd4a7a683f288cf71749f6c0c7b95e0
Mohammad Bilal <mohdbilal16@gmail.com>
2023-08-21 18:16:11 +0530

    created router in vanila JS

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import Store from "./services/Store.js";
 import API from "./services/Api.js";
 import { loadData } from "./services/Menu.js";
+import Router from "./services/Router.js";
 
 
 window.app = {}
 app.store = Store
+app.router = Router
 
 
 window.addEventListener("DOMContentLoaded", async () => {
-     loadData()
+    loadData()
+    app.router.init()
 })
